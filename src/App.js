9835d552df038b827d21e9a0b795327fbe2ed466
Nikhil <nikhil.speedy@gmail.com>
2021-06-30 01:18:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage"
-import SideBar from './components/SideBar/SideBar';
-import  Profile  from './components/Profile/Profile';
+
 import Layout from './Layout';
 
 function App() {
